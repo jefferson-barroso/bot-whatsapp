20f0f2e505c79dcc55a5838873272f83c8b7c414
jefferson.freitas <jefersson.freitas@memora.com.br>
2026-03-10 15:04:52 -0300

    chenge hour

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('qr', (qr) => {
 client.on('ready', () => {
     console.log('Bot pronto!');
 
-    cron.schedule('48 14 * * *', async () => {
+    cron.schedule('07 15 * * *', async () => {
 
         const groupId = '120363038435990275@g.us';
 
